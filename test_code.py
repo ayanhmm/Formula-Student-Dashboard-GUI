5ff1e9d8c3ddd8c61ccb499ea9401507e3211f1e
ayanhmm <117320637+ayanhmm@users.noreply.github.com>
2023-07-24 04:30:05 +0530

    --

---
+++ test_code.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+
+import ast
+import serial
+
+
+
+
+# ser = serial.Serial('/dev/ttyUSB0', 9600)
+# raw_data  = ser.readline().decode()
+
+# float_values = ast.literal_eval(raw_data)
+
+
+port = '/dev/ttyUSB0'
+
+print(raw_data)
